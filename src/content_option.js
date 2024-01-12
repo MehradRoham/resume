5e9116decba6781278f991beae967fbf5c7f73cc
Mohamad-yaghoobi <mohamadyaghoobi1997@gmail.com>
2024-01-12 23:57:43 +0330

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,123 @@
+const logotext = "MR Yaghoobi";
+const meta = {
+    title: "Mohamad yaghoobi",
+    description: "I’m Mohmad yaghoobi  Full stack devloper",
+};
+
+const introdata = {
+    title: "I’Mohamad yaghoobi",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "I’m Mohmad yaghoobi a Full stack devloper",
+    your_img_url: "https://www.uplooder.net/img/image/28/3a68b1e2cc8928143004e4d2ac708b21/html-system-websites-concept-23-2150323534.jpg",
+};
+
+const dataabout = {
+    title: "Professional Experience",
+    aboutme: "A Full Stack Developer with two years of professional experience, specializing in web development, project management, Agile methodology, and quality assurance testing. Adept at coordinating effectively with development teams, clients, and non-technical stakeholders to execute complex web development projects.",
+};
+const worktimeline = [{
+        jobtitle: "Anesthesia Technician",
+        where: "Surgery room",
+        date: "2016-2020",
+    },
+    {
+        jobtitle: "Front-end Developer",
+        where: "Free lancer",
+        date: "2017-2020",
+    },
+    {
+        jobtitle: "Back-end Developer ",
+        where: "Free lancer",
+        date: "2020-2021",
+    },
+    {
+        jobtitle: "React Developer ",
+        where: "Free lancer",
+        date: "2021",
+    },
+];
+
+const skills = [
+    {
+        name: "Html,Css",
+        value: 95,
+    },
+    {
+        name: "Javascript",
+        value: 90,
+    },
+    {
+        name: "Bootstrap",
+        value: 90,
+    },
+    {
+        name: "React",
+        value: 80,
+    },
+    {
+        name: "Node js",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Front-end Developer",
+        description: "  Coordinate with the web development team to design UI for client websites using HTML, JavaScript, and Bootstrap  ",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Support the development of five mobile applications in iOS and Android environments",
+    },
+    {
+        title: "Node.js Developer",
+        description: "Developed several high-performance, scalable Node.js applications",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://www.uplooder.net/img/image/3/50c5b5b02d2d6938916861bb6eae3fc5/pp4ll13f5onw4gqj8ggl.jpg",
+        description: "Front Developer",
+        link: "#",
+    },
+    {
+        img: "https://www.uplooder.net/img/image/49/750c6ac2746b7f5fd32e5e1c1426f293/img01.jpg",
+        description: "Node Js Developer.",
+        link: "#",
+    },
+    {
+        img: "https://www.uplooder.net/img/image/91/66228743bd1b1a2170150d6354473c9d/ab2827f1-a2ea-469f-874f-de59c41af595.jpeg",
+        description: "React Developer.",
+        link: "#",
+    }
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "mohamadyaghoobi1997",
+    YOUR_FONE: "989120277656",
+    description: " ",
+    
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/Mohamad-yaghoobi",
+    
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
